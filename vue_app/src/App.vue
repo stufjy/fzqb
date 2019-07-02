0f9b2563bc2b00c4e981ae75de76938ef726d27b
stufjy <50316437+stufjy@users.noreply.github.com>
2019-07-02 10:01:12 +0800

    Create App.vue

---
+++ vue_app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="app-container">
+    <router-view></router-view>
+ </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style>
+  body{
+    margin:0;
+  }
+  .mint-cell-wrapper{
+    padding:0 !important;
+  }
+</style>
+
